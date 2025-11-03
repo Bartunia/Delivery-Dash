c5b0a4033a8014d12ab7483c5339c39a9f8a272d
Bartunia <wierzchos.bartlomiej@gmail.com>
2025-11-03 23:08:38 +0100

    Updated movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,8 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 10f;
     [SerializeField] float rotationSpeed = 50f;
+    bool isMoving = false;   
+    bool isReversing = false;
 
     void Update()
     {
@@ -17,22 +19,53 @@ public class Movement : MonoBehaviour
         if (Keyboard.current.wKey.isPressed && !Keyboard.current.sKey.isPressed)
         {
             transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
+            isMoving = true;
+            isReversing = false;
         }
         else if (Keyboard.current.sKey.isPressed && !Keyboard.current.wKey.isPressed)
         {
             transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
+            isMoving = true;
+            isReversing = true;
+        }
+        else
+        {
+            isMoving = false;
         }
     }
 
     void RotationInput()
     {
-        if (Keyboard.current.aKey.isPressed && !Keyboard.current.dKey.isPressed)
+        if (isReversing == true)
         {
-            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+            if (Keyboard.current.aKey.isPressed
+            && !Keyboard.current.dKey.isPressed
+            && isMoving == true)
+            {
+                transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+            }
+            else if (Keyboard.current.dKey.isPressed
+            && !Keyboard.current.aKey.isPressed
+            && isMoving == true)
+            {
+                transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+            }
+            return;
         }
-        else if (Keyboard.current.dKey.isPressed && !Keyboard.current.aKey.isPressed)
+        if (isReversing == false)
         {
-            transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+            if (Keyboard.current.aKey.isPressed
+            && !Keyboard.current.dKey.isPressed
+            && isMoving == true)
+            {
+                transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+            }
+            else if (Keyboard.current.dKey.isPressed
+            && !Keyboard.current.aKey.isPressed
+            && isMoving == true)
+            {
+                transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+            }
         }
     }
 }
