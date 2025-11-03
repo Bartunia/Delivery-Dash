d57b92351aeb7c0c6354746a7d0f75bbc28cc188
Bartunia <wierzchos.bartlomiej@gmail.com>
2025-11-03 18:17:00 +0100

    Created movement script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    
+    [SerializeField] float movementSpeed = 10f;
+    [SerializeField] float rotationSpeed = 50f;
+
+    void Update()
+    {
+        DrivingInput();
+        RotationInput();
+    }
+
+    void DrivingInput()
+    {
+        if (Keyboard.current.wKey.isPressed && !Keyboard.current.sKey.isPressed)
+        {
+            transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
+        }
+        else if (Keyboard.current.sKey.isPressed && !Keyboard.current.wKey.isPressed)
+        {
+            transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    void RotationInput()
+    {
+        if (Keyboard.current.aKey.isPressed && !Keyboard.current.dKey.isPressed)
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+        else if (Keyboard.current.dKey.isPressed && !Keyboard.current.aKey.isPressed)
+        {
+            transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+        }
+    }
 }
